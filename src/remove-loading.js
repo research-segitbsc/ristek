3f0eff52372a11c40310d9fdc4da62f52192dd73
reza-nugraha32 <reza040204@gmail.com>
2023-11-13 03:55:21 +0700

    Some changes on structures, new links, and looks

--- src/remove-loading.js
+++
@@ -1,9 +0,0 @@
-$(window).on("load", function () {
-    setTimeout(removeLoader);
-    });
-
-function removeLoader() {
-    $(".loading").fadeOut(500, function () {
-        $(".loading").remove();
-    });
-};
